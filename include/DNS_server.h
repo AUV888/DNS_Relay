e80158d97ca7698f5785fd247a9ce971f25616eb
AUV888 <bigsticks114514@gmail.com>
2026-03-25 22:24:19 +0800

    Add brief descriptions for server socket functions

--- include/DNS_server.h
+++ include/DNS_server.h
@@ -12,7 +12,12 @@ extern int local_socket_fd;
 extern int remote_socket_fd;
 extern int sock_addr_len;
 
+/*@brief Initialized the socket
+ */
 void server_socket_init();
+
+/*@brief Close the socket
+ */
 void server_socket_close();
 void server_mode_blocking_set();
 void server_mode_non_blocking_set();

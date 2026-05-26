2184a47e3c940f1ecfc318a2dcb6131ba18c648f
AUV888 <bigsticks114514@gmail.com>
2026-05-26 22:39:51 +0800

    Add cache

---
+++ include/DNS_cache.h
@@ -0,0 +1,30 @@
+#ifndef DNS_CACHE_H
+#define DNS_CACHE_H
+
+#include <inttypes.h>
+#include <time.h>
+#define TABLE_MAX_SIZE 0x80000
+
+uint32_t hash(const char* str);
+
+typedef struct HASH_NODE {
+    char* domain;
+    uint32_t ipv4;
+    time_t expire_time;
+    struct HASH_NODE* next;
+} node;
+
+typedef struct SET {
+    node* bucket[TABLE_MAX_SIZE];
+    unsigned long size;
+} cache_set;
+
+cache_set* create_hset(void);
+int cache_init(cache_set* s);
+int cache_destroy(cache_set** s);
+int cache_insert(cache_set* s, char* src, uint32_t ip, int ttl);
+int cache_erase(cache_set* s, char* src);
+int cache_clear(cache_set* s);
+int cache_find(cache_set* s, char* src, uint32_t* ip_addr_output);
+unsigned long cache_size(cache_set* s);
+#endif
---
+++ src/DNS_cache.c
@@ -0,0 +1,152 @@
+#include "../include/DNS_cache.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+uint32_t hash(const char* str) {
+    uint32_t hash = 2166136261u;
+
+    while (*str) {
+        hash ^= (uint8_t)(*str++);
+        hash *= 16777619u;
+    }
+
+    return hash;
+}
+
+cache_set* create_hset(void) {
+    cache_set* dest = (cache_set*)malloc(sizeof(cache_set));
+    return dest;
+}
+int cache_init(cache_set* s) {
+    if (!s)
+        return 0;
+    memset(s->bucket, NULL, sizeof(s->bucket));
+    s->size = 0;
+    return 1;
+}
+int cache_destroy(cache_set** s) {
+    if (!s || !(*s))
+        return 0;
+    for (int i = 0; i < TABLE_MAX_SIZE; i++) {
+        node* cur = (*s)->bucket[i];
+        while (cur) {
+            node* tmp = cur->next;
+            free(cur->domain);
+            free(cur);
+            cur = tmp;
+        }
+    }
+    (*s)->size = 0;
+    free(*s);
+    *s = NULL;
+    return 1;
+}
+int cache_insert(cache_set* s, char* src, uint32_t ip, int ttl) {
+    if (!s || !src)
+        return 0;
+
+    uint32_t h = hash(src);
+    node* cur = s->bucket[h];
+
+    while (cur) {
+        if (strcasecmp(cur->domain, src) == 0)
+            return 0;  // 已存在
+        cur = cur->next;
+    }
+
+    node* n = (node*)malloc(sizeof(node));
+    if (!n)
+        return 0;
+
+    n->domain = (char*)malloc(strlen(src) + 1);
+    if (!n->domain) {
+        free(n);
+        return 0;
+    }
+
+    strcpy(n->domain, src);
+    n->ipv4 = ip;
+    n->expire_time = time(NULL) + ttl;
+
+    n->next = s->bucket[h];
+    s->bucket[h] = n;
+    s->size++;
+    return 1;
+}
+
+int cache_erase(cache_set* s, char* src) {
+    if (!s || !src)
+        return 0;
+
+    uint32_t h = hash(src);
+    node* cur = s->bucket[h];
+    node* prev = NULL;
+
+    while (cur) {
+        if (strcasecmp(cur->domain, src) == 0) {
+            if (prev)
+                prev->next = cur->next;
+            else
+                s->bucket[h] = cur->next;
+
+            free(cur->domain);
+            free(cur);
+            s->size--;
+            return 1;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+    return 0;  // 未找到
+}
+
+int cache_clear(cache_set* s) {
+    if (!s)
+        return 0;
+
+    for (int i = 0; i < TABLE_MAX_SIZE; i++) {
+        node* cur = s->bucket[i];
+        while (cur) {
+            node* tmp = cur->next;
+            free(cur->domain);
+            free(cur);
+            cur = tmp;
+        }
+        s->bucket[i] = NULL;
+    }
+    s->size = 0;
+    return 1;
+}
+
+int cache_find(cache_set* s, char* src, uint32_t* ip_addr_output) {
+    if (!s || !src || !ip_addr_output)
+        return 0;
+    uint32_t h = hash(src);
+    node* cur = s->bucket[h];
+    node* prev = NULL;
+    while (cur) {
+        if (strcasecmp(cur->domain, src) == 0) {
+            time_t now = time(NULL);
+            if (cur->expire_time > now) {
+                *ip_addr_output = cur->ipv4;
+                return 1;
+            } else {
+                if (prev)
+                    prev->next = cur->next;
+                else
+                    s->bucket[h] = cur->next;
+
+                free(cur->domain);
+                free(cur);
+                s->size--;
+                return 0;
+            }
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+    return 0;
+}
+
+unsigned long cache_size(cache_set* s) { return s->size; }

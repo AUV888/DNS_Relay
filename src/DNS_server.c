4af8b781f32641dbbc97fd0dcc165e4d26103df5
AUV888 <bigsticks114514@gmail.com>
2026-06-05 22:18:08 +0800

    Add debug logging in server socket functions; log socket initialization, errors, and message processing events

--- src/DNS_server.c
+++ src/DNS_server.c
@@ -15,6 +15,7 @@
 #include "../include/DNS_arguments.h"
 #include "../include/DNS_cache.h"
 #include "../include/DNS_convert.h"
+#include "../include/DNS_debug.h"
 #include "../include/DNS_id.h"
 #include "../include/DNS_struct.h"
 
@@ -25,19 +26,29 @@ int sock_addr_len;
 struct sockaddr_in local_addr;
 struct sockaddr_in remote_addr;
 
-
 cache_set* g_cache;
 
 void server_socket_init() {
+    log_event_t l;
     sock_addr_len = sizeof(local_addr);
 
     local_socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (local_socket_fd < 0) {
-        perror("local socket failed");
+        if (debug_mode) {
+            l = LOCAL_SOCKET_FAILED;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
+        exit(EXIT_FAILURE);
     }
     remote_socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (remote_socket_fd < 0) {
-        perror("remote socket failed");
+        if (debug_mode) {
+            l = REMOTE_SOCKET_FAILED;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
+        exit(EXIT_FAILURE);
     }
 
     memset(&local_addr, 0, sizeof(local_addr));
@@ -53,20 +64,35 @@ void server_socket_init() {
 
     int opt = 1;
     if (setsockopt(local_socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
-        perror("setsockopt SO_REUSEADDR failed");
+        if (debug_mode) {
+            l = SOCKET_OPT_FAILED;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
+        exit(EXIT_FAILURE);
     }
 
     if (bind(local_socket_fd, (const struct sockaddr*)&local_addr, sizeof(local_addr)) < 0) {
-        perror("bind failed");
+        if (debug_mode) {
+            l = SOCKET_BIND_FAILED;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
+        exit(EXIT_FAILURE);
     }
 
     g_cache = create_hset();
     cache_init(g_cache);
 
-    printf("Server socket ready.\n");
+    if (debug_mode) {
+        l = SOCKET_INIT_SUCCESS;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
+    }
 }
 
 void server_socket_close() {
+    log_event_t l;
     if (local_socket_fd != -1) {
         close(local_socket_fd);
         local_socket_fd = -1;
@@ -75,9 +101,20 @@ void server_socket_close() {
         close(remote_socket_fd);
         remote_socket_fd = -1;
     }
+    if (debug_mode) {
+        l = SOCKET_CLOSE_SUCCESS;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
+    }
 }
 
 void server_mode_blocking_set() {
+    log_event_t l;
+    if (debug_mode) {
+        l = BLOCK_MODE_START;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
+    }
     int maxfd = (local_socket_fd > remote_socket_fd ? local_socket_fd : remote_socket_fd) + 1;
 
     while (1) {
@@ -91,53 +128,118 @@ void server_mode_blocking_set() {
         int n = select(maxfd, &rset, NULL, NULL, &tv);
 
         if (n < 0) {
-            if (errno == EINTR)
+            if (errno == EINTR) {
+                if (debug_mode) {
+                    l = BLOCK_MODE_ERRNO_EINTR;
+                    uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                    log_write(pl);
+                }
                 continue;  // interrupted by signal, retry
-            perror("select");
+            }
+            if (debug_mode) {
+                l = BLOCK_MODE_ERRNO_SELECT;
+                uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                log_write(pl);
+            }
             break;
         }
 
         if (n == 0) {
             // Timeout: run periodic housekeeping, then go back to sleep
+            if (debug_mode) {
+                l = BLOCK_MODE_TIMEOUT;
+                uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                log_write(pl);
+            }
             id_map_sweep_timeout();
             continue;
         }
 
         // Dispatch ready file descriptors
-        if (FD_ISSET(local_socket_fd, &rset))
+        if (FD_ISSET(local_socket_fd, &rset)) {
+            if (debug_mode) {
+                l = BLOCK_MODE_LOCAL_RECEIVE;
+                uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                log_write(pl);
+            }
             local_receive();
-        if (FD_ISSET(remote_socket_fd, &rset))
+        }
+        if (FD_ISSET(remote_socket_fd, &rset)) {
+            if (debug_mode) {
+                l = BLOCK_MODE_REMOTE_RECEIVE;
+                uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                log_write(pl);
+            }
             remote_receive();
+        }
     }
 }
 
 void server_mode_non_blocking_set() {
     int flags;
 
+    log_event_t l;
+    if (debug_mode) {
+        l = NON_BLOCK_MODE_START;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
+    }
     flags = fcntl(local_socket_fd, F_GETFL, 0);  // get file status flags
     if (flags < 0) {
-        perror("fcntl F_GETFL local");
+        if (debug_mode) {
+            l = NON_BLOCK_MODE_LOCAL_FGETFL_ERR;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         return;
     }
-    if (fcntl(local_socket_fd, F_SETFL, flags | O_NONBLOCK) < 0)  // set file status flags
-        perror("fcntl F_SETFL local");
+    if (fcntl(local_socket_fd, F_SETFL, flags | O_NONBLOCK) < 0) {  // set file status flags
+        if (debug_mode) {
+            l = NON_BLOCK_MODE_LOCAL_FSETFL_ERR;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
+    }
 
     flags = fcntl(remote_socket_fd, F_GETFL, 0);
     if (flags < 0) {
-        perror("fcntl F_GETFL remote");
+        if (debug_mode) {
+            l = NON_BLOCK_MODE_REMOTE_FGETFL_ERR;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         return;
     }
-    if (fcntl(remote_socket_fd, F_SETFL, flags | O_NONBLOCK) < 0)
-        perror("fcntl F_SETFL remote");
+    if (fcntl(remote_socket_fd, F_SETFL, flags | O_NONBLOCK) < 0) {
+        if (debug_mode) {
+            l = NON_BLOCK_MODE_REMOTE_FSETFL_ERR;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
+    }
 
     time_t last_sweep = time(NULL);
 
     while (1) {
         local_receive();
+        if (debug_mode) {
+            l = NON_BLOCK_MODE_LOCAL_RECEIVE;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         remote_receive();
-
+        if (debug_mode) {
+            l = NON_BLOCK_MODE_REMOTE_RECEIVE;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         time_t now = time(NULL);
         if (now - last_sweep >= 1) {
+            if (debug_mode) {
+                l = NON_BLOCK_SWEEP;
+                uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                log_write(pl);
+            }
             id_map_sweep_timeout();
             last_sweep = now;
         }
@@ -162,18 +264,30 @@ static inline void cache_answers_from_msg(dns_message_t* msg) {
         }
         rr = rr->next;
     }
+    if (debug_mode) {
+        log_event_t l = REMOTE_RECEIVE_CACHED_ANSWER_SUCCESS;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
+    }
 }
 
 void remote_receive() {
     uint8_t buf_recv[BUFFER_SIZE];
 
+    log_event_t l;
     struct sockaddr_in upstream_addr;
     socklen_t upstream_len = sizeof(upstream_addr);
 
     int msg_size = recvfrom(remote_socket_fd, (void*)buf_recv, sizeof(buf_recv), 0,
                             (struct sockaddr*)&upstream_addr, &upstream_len);
-    if (msg_size < 2)
+    if (msg_size < 2) {
+        if (debug_mode) {
+            l = REMOTE_RECEIVE_MSG_SIZE_ERR;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         return;
+    }
 
     uint16_t new_id_be;
     memcpy(&new_id_be, buf_recv, 2);
@@ -182,6 +296,11 @@ void remote_receive() {
     uint16_t orig_id = 0;
     struct sockaddr_in client_addr;
     if (!id_map_find(new_id, &orig_id, &client_addr)) {  // no original id, drop
+        if (debug_mode) {
+            l = REMOTE_RECEIVE_NO_ORIG_ID_DROP;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         return;
     }
 
@@ -191,18 +310,33 @@ void remote_receive() {
     sendto(local_socket_fd, (const void*)buf_recv, msg_size, 0,
            (const struct sockaddr*)&client_addr, sizeof(client_addr));
 
+    if (debug_mode) {
+        l = REMOTE_RECEIVE_SENT_TO_CLIENT;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
+    }
     id_map_erase(new_id);
 
     if (g_cache != NULL) {
+        if (debug_mode) {
+            l = REMOTE_RECEIVE_HAS_GLOBAL_CACHE;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         dns_message_t msg;
         memset(&msg, 0, sizeof(msg));
         dns_message_decode(&msg, buf_recv);
         cache_answers_from_msg(&msg);
         dns_message_free(&msg);
+    } else if (g_cache == NULL && debug_mode) {
+        l = REMOTE_RECEIVE_NO_GLOBAL_CACHE;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
     }
 }
 
 void local_receive() {
+    log_event_t l;
     uint8_t buf_recv[BUFFER_SIZE];
     uint8_t buf_to_send[BUFFER_SIZE];
 
@@ -212,14 +346,26 @@ void local_receive() {
 
     int msg_size = recvfrom(local_socket_fd, (void*)buf_recv, sizeof(buf_recv), 0,
                             (struct sockaddr*)&client_addr, &client_len);
-    if (msg_size <= 0)
+    if (msg_size <= 0) {
+        if (debug_mode) {
+            l = LOCAL_RECEIVE_RECVFROM_FAILED;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            pl |= (INFO_MASK & ntohl(client_addr.sin_addr.s_addr));
+            log_write(pl);
+        }
         return;
+    }
 
     dns_message_t msg;
     memset(&msg, 0, sizeof(msg));
     dns_message_decode(&msg, buf_recv);
 
     if (msg.header == NULL) {
+        if (debug_mode) {
+            l = LOCAL_RECEIVE_DECODE_NULL_HEADER;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         dns_message_free(&msg);
         return;
     }
@@ -230,8 +376,19 @@ void local_receive() {
                               (msg.question->q_type == DNS_TYPE_A);
 
     if (can_resolve_locally && g_cache != NULL) {
+        if (debug_mode) {
+            l = LOCAL_RECEIVE_CAN_RESOLVE_LOCALLY;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         uint32_t ip = 0;
         if (cache_find(g_cache, msg.question->q_name, &ip)) {
+            if (debug_mode) {
+                l = LOCAL_RECEIVE_HIT_CACHE;
+                uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                pl |= (INFO_MASK & ip);
+                log_write(pl);
+            }
             uint8_t ip_addr[4];
             ip_addr[0] = (uint8_t)((ip >> 24) & 0xFF);
             ip_addr[1] = (uint8_t)((ip >> 16) & 0xFF);
@@ -244,8 +401,16 @@ void local_receive() {
                 memset(ans, 0, sizeof(*ans));
                 size_t nlen = strlen(msg.question->q_name);
                 ans->name = (char*)malloc(nlen + 1);
-                if (ans->name)
+                if (ans->name) {
                     memcpy(ans->name, msg.question->q_name, nlen + 1);
+                } else {
+                    if (debug_mode) {
+                        l = LOCAL_RECEIVE_ANS_NAME_MALLOC_FAILED;
+                        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                        log_write(pl);
+                    }
+                    return;
+                }
                 ans->type = DNS_TYPE_A;
                 ans->rr_class = DNS_CLASS_IN;
                 ans->ttl = 300;
@@ -259,17 +424,45 @@ void local_receive() {
                 if (reply_size > 0) {
                     sendto(local_socket_fd, (const void*)buf_to_send, reply_size, 0,
                            (const struct sockaddr*)&client_addr, client_len);
+                } else {
+                    if (debug_mode) {
+                        l = LOCAL_RECEIVE_REPLY_SIZE_ERROR;
+                        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                        log_write(pl);
+                    }
+                }
+            } else {
+                if (debug_mode) {
+                    l = LOCAL_RECEIVE_ANS_MALLOC_FAILED;
+                    uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                    log_write(pl);
                 }
             }
             dns_message_free(&msg);
+            if (debug_mode) {
+                l = LOCAL_RECEIVE_DNS_MESSAGE_FREE_SUCCESS;
+                uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+                log_write(pl);
+            }
             return;
         }
+    }
+
+    if (debug_mode) {
+        l = LOCAL_RECEIVE_CANNOT_HIT_CACHE;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
     }
 
     uint16_t orig_id = msg.header->id;
 
     uint16_t new_id = 0;
     if (!id_map_insert(orig_id, &client_addr, &new_id)) {  // no empty slot, drop
+        if (debug_mode) {
+            l = LOCAL_RECEIVE_NO_EMPTY_SLOT_DROP;
+            uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+            log_write(pl);
+        }
         dns_message_free(&msg);
         return;
     }
@@ -280,5 +473,10 @@ void local_receive() {
     sendto(remote_socket_fd, (const void*)buf_recv, msg_size, 0,
            (const struct sockaddr*)&remote_addr, sizeof(remote_addr));
 
+    if (debug_mode) {
+        l = LOCAL_RECEIVE_SENT_TO_UPSTREAM;
+        uint64_t pl = EVENT_MASK & ((uint64_t)l << 48);
+        log_write(pl);
+    }
     dns_message_free(&msg);
 }
